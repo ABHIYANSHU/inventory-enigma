90bb70076b54b3ad0d349715f9b8726facb67025
Abhinav Srivastava <abhiyanshu11@gmail.com>
2025-01-30 04:09:25 +0530

    Create OrderController.java

---
+++ src/main/java/com/example/inventoryenigma/controller/OrderController.java
@@ -0,0 +1,27 @@
+package com.example.deadlockdemo.controller;
+
+import com.example.deadlockdemo.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/process-a")
+    public String processOrderA() {
+        orderService.processOrder("itemA", "itemB");
+        return "Order A processed!";
+    }
+
+    @PostMapping("/process-b")
+    public String processOrderB() {
+        orderService.processOrder("itemB", "itemA");
+        return "Order B processed!";
+    }
+}
